3557f4380fa5c0461d7952caea39d028ca48240a
Chandrashekhar Tripathi <tripathics@outlook.com>
2023-07-15 12:25:18 +0530

    fix absoute styles of alerts wrapper

--- src/App.js
+++ src/App.js
@@ -9,11 +9,11 @@ function App() {
 
   return (
     <AlertsProvider>
-      <div className="App">
+      <div>
         <header className="App-header">
           <h1>Alerts Demo</h1>
         </header>
-        <main className="App-main w-3/12 min-w-[350px] m-auto">
+        <main className="App-main p-4 m-auto max-w-sm min-w-fit w-full">
           <button
             onClick={() => setShowForm((prev) => !prev)}
             className="mt-6 w-fit bg-gray-200 border-gray-700 border-2 hover:bg-gray-300 text-gray-900 font-bold py-2 px-4 rounded"
--- src/alerts/alert.js
+++ src/alerts/alert.js
@@ -44,7 +44,7 @@ const Alert = ({ message = '', severity = 'info', timeout = 0, handleDismiss = n
 
 const AlertsWrapper = ({ children }) => {
   return (
-    <div className="fixed top-0 right-0 p-4 z-50 pointer-events-none max-w-sm w-4/12">
+    <div className="fixed top-0 right-0 p-4 z-50 pointer-events-none max-w-sm min-w-fit w-full">
       {children}
     </div>
   )
